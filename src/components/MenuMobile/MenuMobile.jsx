6f79c13426aa8c8a1777a8974d63837c8729af39
fakerr-sketch <fm3209742@gmail.com>
2023-02-04 10:22:34 -0300

    animation-duration:add

--- src/components/MenuMobile/MenuMobile.jsx
+++ src/components/MenuMobile/MenuMobile.jsx
@@ -1,24 +1,26 @@
-import "animate.css";
-import {CloseOutline} from "@styled-icons/evaicons-outline/CloseOutline";
-import { MenuWrapper, MenuLinks } from "./styles";
-import {DividerTall} from "@styled-icons/fluentui-system-regular/DividerTall";
+import 'animate.css';
+import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline';
+import { MenuWrapper, MenuLinks } from './styles';
+import { DividerTall } from '@styled-icons/fluentui-system-regular/DividerTall';
 
-function MobileMenu(props){
-    return (
-        <>
-           <MenuWrapper className="animate__animated animate__fadeInRight">
-               <MenuLinks>
-                   <button onClick={() => props.open(false)}><CloseOutline className="closeButton" size="5rem" color="white"/></button>
-                   <a href="#">Premium</a>
-                   <a href="#">Suporte</a>
-                   <a href="#">Baixar</a>
-                   <DividerTall size="2rem" color="white"/>
-                   <a href="#">Inscrever-se</a>
-                   <a href="#">Entrar</a>
-               </MenuLinks>
-           </MenuWrapper>
-        </>
-    )
+function MobileMenu (props) {
+  return (
+    <>
+      <MenuWrapper className='animate__animated animate__fadeInRight'>
+        <MenuLinks>
+          <button onClick={() => props.open(false)}>
+            <CloseOutline className='closeButton' size='5rem' color='white' />
+          </button>
+          <a href='#'>Premium</a>
+          <a href='#'>Suporte</a>
+          <a href='#'>Baixar</a>
+          <DividerTall size='2rem' color='white' />
+          <a href='#'>Inscrever-se</a>
+          <a href='#'>Entrar</a>
+        </MenuLinks>
+      </MenuWrapper>
+    </>
+  );
 }
 
 export default MobileMenu;
--- src/components/MenuMobile/styles.js
+++ src/components/MenuMobile/styles.js
@@ -1,33 +1,34 @@
-import styled from "styled-components";
-import { MenuContainer } from "../Header/styles";
+import styled from 'styled-components';
+import { MenuContainer } from '../Header/styles';
 
 export const MenuWrapper = styled(MenuContainer)`
-    flex-direction: column;
-    justify-content: initial;
-    align-items: initial;
-    position: fixed;
-    background-color: black;
-    right: 0;
-    z-index: 2;
-    height: 100vh;
-    max-width: 90vw;
+  flex-direction: column;
+  justify-content: initial;
+  align-items: initial;
+  position: fixed;
+  background-color: black;
+  right: 0;
+  z-index: 2;
+  height: 100vh;
+  max-width: 90vw;
+  animation-duration: .7s;
 `;
 
-export const MenuLinks= styled.nav`
-    margin-top: 5rem;
-    padding: 1rem;
-    font-size: 3.5rem;
-    display: flex;
-    flex-direction: column;
-    text-align: left;
-    .closeButton{
-        position: absolute;
-        right: 0;
-        top: 1rem;
-    }
-    > a{
-        color: white;
-        margin-bottom: 1.7rem;    
-        font-weight: bolder;
-    }
+export const MenuLinks = styled.nav`
+  margin-top: 5rem;
+  padding: 1rem;
+  font-size: 3.5rem;
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  .closeButton {
+    position: absolute;
+    right: 0;
+    top: 1rem;
+  }
+  > a {
+    color: white;
+    margin-bottom: 1.7rem;
+    font-weight: bolder;
+  }
 `;
